496f6cece2dcb9da2cb750eeca076bdbd8990f34
PieterBrandsen <pieterbrandsen2@gmail.com>
2023-01-22 20:19:10 +0100

    Moved deletePath

---
+++ src/deletePath.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { join, dirname } = require('path');
+const { fileURLToPath } = require('url');
+
+const minimist = require('minimist')
+const argv = minimist(process.argv.slice(2));
+console.dir(argv);
+
+const whisperPath = join(__dirname, '../whisper/');
+let statsPath = argv.statsPath;
+if (!statsPath) {
+  console.error('Please provide a path to the stats');
+  process.exit(1);
+}
+
+if (!fs.existsSync(whisperPath)) {
+    console.log("No whisper folder found, manually delete the stats are not working while whisper export is disabled.");
+    process.exit(1);
+}
+else if (statsPath.startsWith('.')) {
+  console.log("Please provide a path without a leading dot");
+  process.exit(1);
+}
+else if (statsPath.endsWith('.')) {
+  console.log("Please provide a path without a trailing dot");
+  process.exit(1);
+}
+statsPath = statsPath.split('.').join('/');
+
+function deletePath(path) {
+  if (fs.existsSync(path)) {
+    fs.rm(path, { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.log(`Deleted ${path}`);
+    });
+    return;
+  }
+  console.log(`Path not found: ${path}`);
+}
+deletePath(join(whisperPath, statsPath));
