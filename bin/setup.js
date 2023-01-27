57168115e7e29f8f4bd3972c1ba9ca4511b29c7c
PieterBrandsen <pieterbrandsen2@gmail.com>
2023-01-27 22:36:12 +0100

    Added console.log for compose file

--- bin/setup.js
+++ bin/setup.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
-require('dotenv').config()
 const { execSync } = require('child_process');
+const { join } = require('path');
+require('dotenv').config({ path: join(__dirname, '../.env') })
 
 async function run() {
     await import('../src/setup/index.js');
 }
 
 if (process.argv[2] === 'stop') {
+    console.log(process.env.COMPOSE_FILE)
+    console.log()
+    console.log(process.env)
     execSync('docker-compose stop');
 }
 else run();
