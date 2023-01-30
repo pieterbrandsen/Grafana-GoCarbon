6b0586e18b1c9adaae8925abfb6f8d7b94ad57d1
Pieter <pieterbrandsen2@gmail.com>
2023-01-30 12:28:35 +0000

    Linting

--- bin/setup.js
+++ bin/setup.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
 const { join } = require('path');
-require('dotenv').config({ path: join(__dirname, '../.env') })
+require('dotenv').config({ path: join(__dirname, '../.env') });
 
 async function run() {
-    await import('../src/setup/index.js');
+  await import('../src/setup/index.js');
 }
 
 if (process.argv[2] === 'stop') {
-    console.log(process.env.COMPOSE_FILE)
-    console.log()
-    console.log(process.env)
-    execSync('docker-compose stop');
-}
-else run();
+  console.log(process.env.COMPOSE_FILE);
+  console.log();
+  console.log(process.env);
+  execSync('docker-compose stop');
+} else run();
